31d9e872795535aa4b7ebacc0cf05714dc293c20
anggario21d <anggario_21@apps.ipb.ac.id>
2024-06-24 17:30:41 +0700

    finishing weather app

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { ActivityIndicator, StyleSheet, View, Text } from "react-native";
+import WeatherInfo from "./components/weatherInfo";
+import WeatherSearch from "./components/weatherSearch";
+import axios from "axios";
+import { API_KEY, BASE_URL } from "./components/constant";
 
-export default function App() {
+const App = () => {
+  const [weatherData, setWeatherData] = useState();
+  const [status, setStatus] = useState("");
+
+  const renderComponent = () => {
+    switch (status) {
+      case "loading":
+        return <ActivityIndicator size="large" />;
+      case "success":
+        return <WeatherInfo weatherData={weatherData} />;
+      case "error":
+        return <Text>Something went wrong. Please try again with a correct city name.</Text>;
+      default:
+        return;
+    }
+  };
+
+  const searchWeather = (location) => {
+    setStatus("loading");
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data;
+        data.visibility /= 1000;
+        data.visibility = data.visibility.toFixed(2);
+        data.main.temp -= 273.15;
+        data.main.temp = data.main.temp.toFixed(2);
+        setWeatherData(data);
+        setStatus("success");
+      })
+      .catch((error) => {
+        setStatus("error");
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WeatherSearch searchWeather={searchWeather} />
+      <View style={styles.marginTop20}>{renderComponent()}</View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  marginTop20: {
+    marginTop: 20,
   },
 });
+
+export default App;
---
+++ components/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+export const API_KEY = "f6fef1c8f66ac6b1efca1d32481edda9";
